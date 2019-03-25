7fa92e0e99d6fe1725f7eabba2a76714d89ad628
sam20gh <sam20gh@gmail.com>
2019-03-25 15:38:41 +0000

    First commit

--- app/models/Question.rb
+++ app/models/Question.rb
@@ -1,3 +1,16 @@
 class Question < ActiveRecord::Base
+
+    def self.get_clubs
+        url = 'https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple'
+        data = JSON.parse(response)
+
+        data["teams"].each do |t|
+          Club.find_or_create_by(name: t["name"], logo: t["crestUrl"],
+            address: t["address"], phone: t["phone"],
+            website: t["website"], email: t["email"],
+            founded: t["founded"], venue: t["venue"],
+            id: t["id"])
+        end
+      end
 end
     
