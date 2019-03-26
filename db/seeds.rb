51a63b4c1de925c35abf5bd7417506fa137e2597
oli-gra <oliver.grant@gmail.com>
2019-03-26 09:51:23 +0000

    added welcome ui and basic question display

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,24 @@
-oli = User.new(name:'Oli')
-sam = User.new(name:'Sam')
+User.destroy_all
+Question.destroy_all
+Leaderboard.destroy_all
 
-q1 = Question.new(question:'Who is the only King without a tash in the standard 52 card deck?',answer:'Hearts',wrong_1:'Diamonds',wrong_2:'Spades', wrong_3:'Clubs')
+User.create(name:'Oli')
+User.create(name:'Sam')
+
+Question.create(
+  question: 'Who is the only King without a tash in the standard 52 card deck?',
+  category: 'General Knowledge',
+  difficulty: 'medium',
+  answer: 'Hearts',
+  wrong_1: 'Diamonds',
+  wrong_2: 'Spades',
+  wrong_3: 'Clubs')
+
+Question.create(
+  question: 'A doctor with a PhD is a doctor of what?',
+  category: 'General Knowledge',
+  difficulty: 'medium',
+  answer: 'Philosophy',
+  wrong_1: 'Medicine',
+  wrong_2: 'Phrenology',
+  wrong_3: 'Science')
