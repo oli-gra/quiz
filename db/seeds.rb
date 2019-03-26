725bb601ca5fd68f7b01d449422417f01b6e391f
oli-gra <45819636+oli-gra@users.noreply.github.com>
2019-03-26 17:39:59 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,3 @@ Question.all.each {|q| q.update(wrong_1: coder.decode(q.wrong_1))}
 Question.all.each {|q| q.update(wrong_2: coder.decode(q.wrong_2))}
 Question.all.each {|q| q.update(wrong_3: coder.decode(q.wrong_3))}
 Question.all.each {|q| q.update(question: coder.decode(q.question))}
-
